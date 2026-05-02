b4100ed9426f779aba1810a4b7704de39ecb3571
Maycon Bruno <mayconramos.codeprojects@gmail.com>
2026-05-02 13:11:53 -0300

    fix appConfig

--- src/main/java/com/integration/shopifybling/config/AppConfig.java
+++ src/main/java/com/integration/shopifybling/config/AppConfig.java
@@ -1,20 +1,18 @@
 package com.integration.shopifybling.config;
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
-
-import java.time.Duration;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder
-                .connectTimeout(Duration.ofSeconds(5))
-                .readTimeout(Duration.ofSeconds(10))
-                .build();
+    public RestTemplate restTemplate() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(5000);  // 5 segundos
+        factory.setReadTimeout(10000);    // 10 segundos
+        return new RestTemplate(factory);
     }
 }
